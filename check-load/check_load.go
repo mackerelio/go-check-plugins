a80550a036420a1c7275698261303e56309dca11
tatsuru <w.tatsuru@gmail.com>
2015-05-25 21:54:02 +0900

    add check-load

---
+++ check-load/check_load.go
@@ -0,0 +1,111 @@
+package main
+
+// // for getloadavg(2)
+/*
+#include <stdlib.h>
+*/
+import "C"
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/jessevdk/go-flags"
+)
+
+var opts struct {
+	WarningThreshold  string `short:"w" long:"warning" required:"true" value-name:"WL1,WL5,WL15" description:"Warning threshold for loadavg1,5,15"`
+	CriticalThreshold string `short:"c" long:"critical" required:"true" value-name:"CL1,CL5,CL15" description:"Critical threshold for loadavg1,5,15"`
+	PerCpu            bool   `short:"r" long:"percpu" default:"false" description:"Divide the load averages by cpu count"`
+}
+
+type checkStatus int
+
+const (
+	ok checkStatus = iota
+	warning
+	critical
+	unknown
+)
+
+func (chSt checkStatus) String() string {
+	switch {
+	case chSt == 0:
+		return "OK"
+	case chSt == 1:
+		return "WARNING"
+	case chSt == 2:
+		return "CRITICAL"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+func parseThreshold(str string) ([3]float64, error) {
+	thresholds := [3]float64{0, 0, 0}
+
+	thSt := strings.Split(str, ",")
+	if len(thSt) != 3 {
+		return thresholds, errors.New("Threshold must be comma-separated 3 numbers")
+	}
+
+	var err error
+	for i, v := range thSt {
+		thresholds[i], err = strconv.ParseFloat(v, 64)
+		if err != nil {
+			return thresholds, err
+		}
+	}
+	return thresholds, nil
+}
+
+func main() {
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(int(unknown))
+	}
+
+	wload, err := parseThreshold(opts.WarningThreshold)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(int(unknown))
+	}
+	cload, err := parseThreshold(opts.CriticalThreshold)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(int(unknown))
+	}
+
+	var (
+		cLoadavg [3]C.double
+		loadavg  [3]float64
+	)
+	C.getloadavg(&cLoadavg[0], 3)
+	for i, v := range cLoadavg {
+		loadavg[i] = float64(v)
+	}
+
+	result := ok
+	for i := 0; i < 3; i++ {
+		load := loadavg[i]
+		if opts.PerCpu {
+			numCPU := runtime.NumCPU()
+			load = load / float64(numCPU)
+		}
+		if load > cload[i] {
+			result = critical
+			break
+		}
+		if load > wload[i] {
+			result = warning
+		}
+	}
+
+	fmt.Printf("%s - load average: %.2f, %.2f, %.2f\n", result.String(), loadavg[0], loadavg[1], loadavg[2])
+	os.Exit(int(result))
+}
