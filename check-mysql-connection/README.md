975a200c62330a3dacbcb7b0e349fc2db61da4ea
Songmu <y.songmu@gmail.com>
2015-10-25 16:34:37 +0900

    update check-mysql-connection.go

--- check-mysql-connection/README.md
+++ check-mysql-connection/README.md
@@ -7,6 +7,6 @@ Checks the number of MySQL connections.
 
 ```
 [plugin.checks.mysql-connection]
-command = "/path/to/check-mysql-connection -host=127.0.0.1 -port=3306 -username=USER -password=PASSWORD -warn=250 -crit=280
+command = "/path/to/check-mysql-connection --host=127.0.0.1 --port=3306 --user=USER --password=PASSWORD --warning=250 --critical=280
 ```
 
--- check-mysql-connection/check-mysql-connection.go
+++ check-mysql-connection/check-mysql-connection.go
@@ -1,59 +1,57 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+	"github.com/mackerelio/checkers"
 	"github.com/ziutek/mymysql/mysql"
 	_ "github.com/ziutek/mymysql/native"
-	"os"
 )
 
-// The exit status of the commands
-const (
-	OK       = 0
-	WARNING  = 1
-	CRITICAL = 2
-	UNKNOWN  = 3
-)
+var opts struct {
+	Host string `short:"h" long:"host" default:"localhost" description:"Hostname"`
+	Port string `short:"p" long:"port" default:"3306" description:"Port"`
+	User string `short:"u" long:"user" default:"root" description:"Username"`
+	Pass string `short:"P" long:"password" default:"" description:"Password"`
+	Crit int64  `short:"c" long:"critical" default:"250" description:"critical if the number of connection is over"`
+	Warn int64  `short:"w" long:"warning" default:"200" description:"warning if the number of connection is over"`
+}
 
 func main() {
-	optHost := flag.String("host", "localhost", "Hostname")
-	optPort := flag.String("port", "3306", "Port")
-	optUser := flag.String("username", "root", "Username")
-	optPass := flag.String("password", "", "Password")
-	optCrit := flag.Int("crit", 250, "critical if the number of connection is over")
-	optWarn := flag.Int("warn", 200, "warning if the number of connection is over")
-	flag.Parse()
+	ckr := run(os.Args[1:])
+	ckr.Name = "MySQL Connection"
+	ckr.Exit()
+}
 
-	target := fmt.Sprintf("%s:%s", *optHost, *optPort)
-	db := mysql.New("tcp", "", target, *optUser, *optPass, "")
-	err := db.Connect()
+func run(args []string) *checkers.Checker {
+	_, err := flags.ParseArgs(&opts, args)
 	if err != nil {
-		fmt.Println("UNKNOWN: couldn't connect DB")
-		os.Exit(UNKNOWN)
+		os.Exit(1)
+	}
+	target := fmt.Sprintf("%s:%s", opts.Host, opts.Port)
+	db := mysql.New("tcp", "", target, opts.User, opts.Pass, "")
+	err = db.Connect()
+	if err != nil {
+		return checkers.Unknown("couldn't connect DB")
 	}
 	defer db.Close()
 
-	rows, res, err := db.Query("show global status like 'Threads_Connected'")
+	rows, res, err := db.Query("SHOW GLOBAL STATUS LIKE 'Threads_Connected'")
 	if err != nil {
-		fmt.Println("UNKNOWN: couldn't execute query")
-		os.Exit(UNKNOWN)
+		return checkers.Unknown("couldn't execute query")
 	}
 
 	idxValue := res.Map("Value")
-	threadsConnected := rows[0].Int(idxValue)
+	threadsConnected := rows[0].Int64(idxValue)
 
-	if threadsConnected > *optCrit {
-		msg := fmt.Sprintf("CRITICAL: %d connections", threadsConnected)
-		fmt.Println(msg)
-		os.Exit(CRITICAL)
-	} else if threadsConnected > *optWarn {
-		msg := fmt.Sprintf("WARNING: %d connections", threadsConnected)
-		fmt.Println(msg)
-		os.Exit(WARNING)
+	checkSt := checkers.OK
+	msg := fmt.Sprintf("%d connections", threadsConnected)
+	if threadsConnected > opts.Crit {
+		checkSt = checkers.CRITICAL
+	} else if threadsConnected > opts.Warn {
+		checkSt = checkers.WARNING
 	}
-
-	msg := fmt.Sprintf("OK: %d connections", threadsConnected)
-	fmt.Println(msg)
-	os.Exit(OK)
+	return checkers.NewChecker(checkSt, msg)
 }
