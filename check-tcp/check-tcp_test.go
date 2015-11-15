28c056375db0a1c079530c98932f51467e62735b
Songmu <y.songmu@gmail.com>
2015-11-15 23:54:39 +0900

    add tests

--- check-tcp/check-tcp_test.go
+++ check-tcp/check-tcp_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 
+	"github.com/mackerelio/checkers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,4 +13,18 @@ func TestEscapedString(t *testing.T) {
 	assert.Equal(t, "ho\rge", escapedString(`ho\rge`), "something went wrong")
 	assert.Equal(t, "ho\\oge", escapedString(`ho\oge`), "something went wrong")
 	assert.Equal(t, "", escapedString(``), "something went wrong")
+}
+
+func TestTLS(t *testing.T) {
+	opts, err := parseArgs([]string{"-S", "-H", "www.verisign.com", "-p", "443"})
+	assert.Equal(t, nil, err, "no errors")
+	ckr := opts.run()
+	assert.Equal(t, checkers.OK, ckr.Status, "should be OK")
+}
+
+func TestFTP(t *testing.T) {
+	opts, err := parseArgs([]string{"--service=ftp", "-H", "ftp.iij.ad.jp"})
+	assert.Equal(t, nil, err, "no errors")
+	ckr := opts.run()
+	assert.Equal(t, checkers.OK, ckr.Status, "should be OK")
 }
