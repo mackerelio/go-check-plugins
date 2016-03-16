0b088810a6d06d9bfa0d547873ddeffbc1dcc4d4
a-know <a.know.3373@gmail.com>
2016-03-16 19:32:05 +0900

    Fix warn-* -> warning-*

--- check-uptime/check-uptime.go
+++ check-uptime/check-uptime.go
@@ -11,9 +11,9 @@ import (
 )
 
 var opts struct {
-	WarnUnder *float64 `short:"w" long:"warn-under" value-name:"N" description:"Trigger a warning if under the seconds"`
+	WarnUnder *float64 `short:"w" long:"warning-under" value-name:"N" description:"Trigger a warning if under the seconds"`
 	CritUnder *float64 `short:"c" long:"critical-under" value-name:"N" description:"Trigger a critial if under the seconds"`
-	WarnOver  *float64 `short:"W" long:"warn-over" value-name:"N" description:"Trigger a warning if over the seconds"`
+	WarnOver  *float64 `short:"W" long:"warning-over" value-name:"N" description:"Trigger a warning if over the seconds"`
 	CritOver  *float64 `short:"C" long:"critical-over" value-name:"N" description:"Trigger a critical if over the seconds"`
 }
 
