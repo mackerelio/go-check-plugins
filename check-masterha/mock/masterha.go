ccd6c3625f1375224a0f1a2821a3e58d44946b22
karupanerura <karupa@cpan.org>
2016-03-24 16:39:52 +0900

    write go doc for mock executer

--- check-masterha/mock/masterha.go
+++ check-masterha/mock/masterha.go
@@ -4,6 +4,8 @@ import (
 	"github.com/mackerelio/checkers"
 )
 
+
+// Executer is dummy executer for testing
 type Executer struct {
 	CommandName string
 	CommandArgs []string
@@ -12,14 +14,17 @@ type Executer struct {
 	ParseResult string
 }
 
+// MakeCommandName is dummy command name maker for testing
 func (e *Executer) MakeCommandName() string {
 	return e.CommandName
 }
 
+// MakeCommandArgs is dummy command arguments maker for testing
 func (e *Executer) MakeCommandArgs() []string {
 	return e.CommandArgs
 }
 
+// Parse is dummy parse method for testing
 func (e *Executer) Parse(result string) (checkers.Status, string) {
 	e.CommandResult = result
 	return e.Status, e.ParseResult
