a9104feb4c49a78efa6d2b387f019f986b923da4
Songmu <y.songmu@gmail.com>
2016-04-19 12:35:19 +0900

    backward compatibility

--- check-uptime/check-uptime.go
+++ check-uptime/check-uptime.go
@@ -11,10 +11,12 @@ import (
 )
 
 var opts struct {
-	WarnUnder *float64 `short:"w" long:"warning-under" value-name:"N" description:"Trigger a warning if under the seconds"`
-	CritUnder *float64 `short:"c" long:"critical-under" value-name:"N" description:"Trigger a critial if under the seconds"`
-	WarnOver  *float64 `short:"W" long:"warning-over" value-name:"N" description:"Trigger a warning if over the seconds"`
-	CritOver  *float64 `short:"C" long:"critical-over" value-name:"N" description:"Trigger a critical if over the seconds"`
+	WarnUnder    *float64 `long:"warn-under" value-name:"N" description:"(DEPRECATED) Trigger a warning if under the seconds"`
+	WarningUnder *float64 `short:"w" long:"warning-under" value-name:"N" description:"Trigger a warning if under the seconds"`
+	CritUnder    *float64 `short:"c" long:"critical-under" value-name:"N" description:"Trigger a critial if under the seconds"`
+	WarnOver     *float64 `long:"warn-over" value-name:"N" description:"(DEPRECATED) Trigger a warning if over the seconds"`
+	WarningOver  *float64 `short:"W" long:"warning-over" value-name:"N" description:"Trigger a warning if over the seconds"`
+	CritOver     *float64 `short:"C" long:"critical-over" value-name:"N" description:"Trigger a critical if over the seconds"`
 }
 
 func main() {
@@ -32,11 +34,20 @@ func run(args []string) *checkers.Checker {
 	if err != nil {
 		return checkers.Unknown(fmt.Sprintf("Faild to fetch uptime metrics: %s", err))
 	}
+
+	// for backward compatibility
+	if opts.WarnUnder != nil && opts.WarningUnder == nil {
+		opts.WarningUnder = opts.WarnUnder
+	}
+	if opts.WarnOver != nil && opts.WarningOver == nil {
+		opts.WarningOver = opts.WarnOver
+	}
+
 	checkSt := checkers.OK
-	if opts.WarnUnder != nil && *opts.WarnUnder > ut {
+	if opts.WarningUnder != nil && *opts.WarningUnder > ut {
 		checkSt = checkers.WARNING
 	}
-	if opts.WarnOver != nil && *opts.WarnOver < ut {
+	if opts.WarningOver != nil && *opts.WarningOver < ut {
 		checkSt = checkers.WARNING
 	}
 	if opts.CritUnder != nil && *opts.CritUnder > ut {
