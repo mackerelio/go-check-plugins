c686bb5a9faf149bfe85f5bed8d562bbef0e4104
Songmu <y.songmu@gmail.com>
2017-03-11 21:39:09 +0900

    bundle common processing in MCUtil.pm more

--- tool/MCUtil.pm
+++ tool/MCUtil.pm
@@ -6,6 +6,9 @@ use utf8;
 
 use IPC::Cmd qw/run/;
 use Carp qw/croak/;
+use ExtUtils::MakeMaker qw/prompt/;
+use JSON::PP qw/decode_json/;
+use version;
 
 use parent 'Exporter';
 
@@ -14,10 +17,11 @@ our @EXPORT = qw/
     http_get
     debugf infof warnf errorf
     replace slurp spew
+    parse_version decide_next_version suggest_next_version is_valid_version
+    last_release merged_prs build_pull_request_body
     scope_guard/;
 
 sub DEBUG() { $ENV{MC_RELENG_DEBUG} }
-
 
 sub command_with_exit_code {
     say('+ '. join ' ', @_) if DEBUG;
@@ -105,6 +109,69 @@ sub replace {
     my ($file, $code) = @_;
     my $content = $code->(slurp($file));
     spew($file, $content);
+}
+
+## version utils
+sub parse_version {
+    my $ver = shift;
+    my ($major, $minor, $patch) = $ver =~ /^([0-9]+)\.([0-9]+)\.([0-9]+)$/;
+    ($major, $minor, $patch)
+}
+
+sub suggest_next_version {
+    my $ver = shift;
+    my ($major, $minor, $patch) = parse_version($ver);
+    join '.', $major, ++$minor, 0;
+}
+
+sub is_valid_version {
+    my $ver = shift;
+    my ($major) = parse_version($ver);
+    defined $major;
+}
+
+sub decide_next_version {
+    my $current_version = shift;
+    my $next_version = suggest_next_version($current_version);
+    $next_version = prompt("next version", $next_version);
+
+    if (!is_valid_version($next_version)) {
+        die qq{"$next_version" is invalid version string\n};
+    }
+    if (version->parse($next_version) < version->parse($current_version)) {
+        die qq{"$next_version" is smaller than current version "$current_version"\n};
+    }
+    $next_version;
+}
+
+## git utils
+sub last_release {
+    my @out = `git tag`;
+
+    my ($tag) =
+        sort { version->parse($b) <=> version->parse($a) }
+        map {/^v([0-9]+(?:\.[0-9]+){2})$/; $1 || ()}
+        map {chomp; $_} @out;
+    $tag;
+}
+
+sub merged_prs {
+    my $current_tag = shift;
+
+    my $data = eval { decode_json scalar `ghch -f v$current_tag` };
+    if ($! || $@) {
+        die "parse json failed: $@";
+    }
+    return grep {$_->{title} !~ /\[nitp?\]/i} @{ $data->{pull_requests} };
+}
+
+sub build_pull_request_body {
+    my ($next_version, @releases) = @_;
+    my $body = "Release version $next_version\n\n";
+    for my $rel (@releases) {
+        $body .= sprintf "- %s #%s\n", $rel->{title}, $rel->{number};
+    }
+    $body;
 }
 
 # scope_guard
