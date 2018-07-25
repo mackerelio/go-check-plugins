894669bfd89814b7d50b53aec86abf6d20bb8337
shiimaxx <yoshima@shiimaxx.com>
2018-07-25 18:12:04 +0900

    Add check-smtp

---
+++ check-smtp/lib/check-smtp.go
@@ -0,0 +1,120 @@
+package checksmtp
+
+import (
+	"os"
+
+	"net/smtp"
+
+	"fmt"
+
+	"crypto/tls"
+
+	"net"
+
+	"time"
+
+	"github.com/jessevdk/go-flags"
+	"github.com/mackerelio/checkers"
+)
+
+var opts struct {
+	Host     string  `short:"H" long:"host" default:"localhost" description:"Hostname"`
+	Port     string  `short:"p" long:"port" default:"25" description:"Port"`
+	FQDN     string  `short:"F" long:"fqdn" description:"FQDN used for HELO"`
+	SMTPS    bool    `short:"s" long:"smtps" description:"Use SMTP over TLS"`
+	StartTLS bool    `short:"S" long:"starttls" description:"Use STARTTLS"`
+	Auth     string  `short:"A" long:"authmech" description:"SMTP AUTH Authentication Mechanisms (only PLAIN supported)"`
+	User     string  `short:"U" long:"authuser" description:"SMTP AUTH username"`
+	Password string  `short:"P" long:"authpassword" description:"SMTP AUTH password"`
+	Warning  float64 `short:"w" long:"warning" default:"3" description:"Warning threshold (sec)"`
+	Critical float64 `short:"c" long:"critical" default:"5" description:"Critical threshold (sec)"`
+	Timeout  int     `short:"t" long:"timeout" default:"10" description:"Timeout (sec)"`
+}
+
+// Do the plugin
+func Do() {
+	ckr := run(os.Args[1:])
+	ckr.Name = "SMTP"
+	ckr.Exit()
+}
+
+func makeConn(host, port string, timeout int, isSMTPS bool, tlsConfig *tls.Config) (net.Conn, error) {
+	d := net.Dialer{Timeout: time.Duration(timeout) * time.Second}
+	if isSMTPS {
+		return tls.DialWithDialer(&d, "tcp", fmt.Sprintf("%s:%s", host, port), tlsConfig)
+	}
+	return d.Dial("tcp", fmt.Sprintf("%s:%s", opts.Host, opts.Port))
+}
+
+func run(args []string) *checkers.Checker {
+	_, err := flags.ParseArgs(&opts, args)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if opts.Auth != "" && opts.Auth != "PLAIN" {
+		return checkers.Unknown("invalid SMTP AUTH Authentication Mechanisms (only PLAIN supported)")
+	}
+
+	fqdn := opts.FQDN
+	if fqdn == "" {
+		localHostName, err := os.Hostname()
+		if err != nil {
+			return checkers.Unknown(err.Error())
+		}
+		fqdn = localHostName
+	}
+
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         opts.Host,
+	}
+
+	conn, err := makeConn(opts.Host, opts.Port, opts.Timeout, opts.SMTPS, tlsConfig)
+	if err != nil {
+		return checkers.Critical(err.Error())
+	}
+
+	stTime := time.Now()
+
+	c, err := smtp.NewClient(conn, opts.Host)
+	if err != nil {
+		return checkers.Critical(err.Error())
+	}
+	defer c.Quit()
+
+	if err := c.Hello(fqdn); err != nil {
+		return checkers.Critical(err.Error())
+	}
+
+	if opts.StartTLS {
+		if err := c.StartTLS(tlsConfig); err != nil {
+			return checkers.Critical(err.Error())
+		}
+	}
+
+	if opts.Auth == "PLAIN" {
+		auth := smtp.PlainAuth("", opts.User, opts.Password, opts.Host)
+		if err := c.Auth(auth); err != nil {
+			return checkers.Critical(err.Error())
+		}
+	}
+
+	if err := c.Mail(""); err != nil {
+		c.Reset()
+		return checkers.Critical(err.Error())
+	}
+	c.Reset()
+
+	elapsed := time.Since(stTime)
+
+	msg := fmt.Sprintf("%.3f seconds response time", elapsed.Seconds())
+
+	if elapsed.Seconds() > opts.Critical {
+		return checkers.Critical(msg)
+	} else if elapsed.Seconds() > opts.Warning {
+		return checkers.Warning(msg)
+	} else {
+		return checkers.Ok(msg)
+	}
+}
---
+++ check-smtp/lib/check-smtp_test.go
@@ -0,0 +1,114 @@
+package checksmtp
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"net/textproto"
+	"strings"
+)
+
+var responseDefault = []string{
+	"220 mail.example.com ESMTP unknown",
+	"250 mail.example.com",
+	"250 OK",
+	"250 OK",
+	"221 Bye",
+}
+
+type mockSMTPServer struct {
+	listener  net.Listener
+	delay     int
+	responses []string
+}
+
+func (m *mockSMTPServer) runServe() error {
+	doneCh := make(chan struct{})
+	errCh := make(chan error)
+	go func() {
+		time.Sleep(time.Duration(m.delay) * time.Second)
+
+		conn, err := m.listener.Accept()
+		if err != nil {
+			errCh <- err
+			return
+		}
+		tc := textproto.NewConn(conn)
+
+		for _, res := range m.responses {
+			if err := tc.PrintfLine(res); err != nil {
+				errCh <- err
+				return
+			}
+		}
+		doneCh <- struct{}{}
+	}()
+
+	select {
+	case <-doneCh:
+		return nil
+	case err := <-errCh:
+		return err
+	}
+}
+
+func TestSMTP_Default(t *testing.T) {
+	var cases = []struct {
+		name      string
+		argv      []string
+		delay     int
+		expected  string
+		responses []string
+	}{
+		{
+			name:      "default",
+			argv:      []string{},
+			responses: responseDefault,
+			expected:  "OK",
+		},
+		{
+			name:      "warning",
+			argv:      []string{"--warning", "1"},
+			delay:     1,
+			responses: responseDefault,
+			expected:  "WARNING",
+		},
+		{
+			name:      "critical",
+			argv:      []string{"--critical", "1"},
+			delay:     1,
+			responses: responseDefault,
+			expected:  "CRITICAL",
+		},
+		{
+			name:      "timeout",
+			argv:      []string{"--timeout", "-1"},
+			responses: responseDefault,
+			expected:  "CRITICAL",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			ln, err := net.Listen("tcp", "127.0.0.1:0")
+			defer ln.Close()
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+			s := mockSMTPServer{
+				listener:  ln,
+				responses: c.responses,
+				delay:     c.delay,
+			}
+			go s.runServe()
+			port := strings.Split(ln.Addr().String(), ":")[1]
+			argv := []string{"--host", "127.0.0.1", "--port", port}
+			argv = append(argv, c.argv...)
+			ckr := run(argv)
+			if ckr.Status.String() != c.expected {
+				t.Errorf("%s: %s", ckr.Status.String(), ckr.Message)
+			}
+		})
+	}
+}
