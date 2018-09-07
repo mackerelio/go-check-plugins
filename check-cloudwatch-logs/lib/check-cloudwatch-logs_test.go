877637da38de35c5810f21189cc469a98d900b12
itchyny <itchyny@hatena.ne.jp>
2018-09-07 15:35:24 +0900

    test state file contents of cloudwatch-logs

--- check-cloudwatch-logs/lib/check-cloudwatch-logs_test.go
+++ check-cloudwatch-logs/lib/check-cloudwatch-logs_test.go
@@ -1,6 +1,10 @@
 package checkcloudwatchlogs
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -33,12 +37,14 @@ func createMockService() cloudwatchlogsiface.CloudWatchLogsAPI {
 				NextToken: aws.String("1"),
 				Events: []*cloudwatchlogs.FilteredLogEvent{
 					{
-						EventId: aws.String("event-id-0"),
-						Message: aws.String("message-0"),
+						EventId:   aws.String("event-id-0"),
+						Message:   aws.String("message-0"),
+						Timestamp: aws.Int64(0),
 					},
 					{
-						EventId: aws.String("event-id-1"),
-						Message: aws.String("message-1"),
+						EventId:   aws.String("event-id-1"),
+						Message:   aws.String("message-1"),
+						Timestamp: aws.Int64(1),
 					},
 				},
 			},
@@ -46,24 +52,28 @@ func createMockService() cloudwatchlogsiface.CloudWatchLogsAPI {
 				NextToken: aws.String("2"),
 				Events: []*cloudwatchlogs.FilteredLogEvent{
 					{
-						EventId: aws.String("event-id-2"),
-						Message: aws.String("message-2"),
+						EventId:   aws.String("event-id-2"),
+						Message:   aws.String("message-2"),
+						Timestamp: aws.Int64(2),
 					},
 					{
-						EventId: aws.String("event-id-3"),
-						Message: aws.String("message-3"),
+						EventId:   aws.String("event-id-3"),
+						Message:   aws.String("message-3"),
+						Timestamp: aws.Int64(3),
 					},
 					{
-						EventId: aws.String("event-id-4"),
-						Message: aws.String("message-4"),
+						EventId:   aws.String("event-id-4"),
+						Message:   aws.String("message-4"),
+						Timestamp: aws.Int64(4),
 					},
 				},
 			},
 			"2": &cloudwatchlogs.FilterLogEventsOutput{
 				Events: []*cloudwatchlogs.FilteredLogEvent{
 					{
-						EventId: aws.String("event-id-5"),
-						Message: aws.String("message-5"),
+						EventId:   aws.String("event-id-5"),
+						Message:   aws.String("message-5"),
+						Timestamp: aws.Int64(5),
 					},
 				},
 			},
@@ -72,11 +82,21 @@ func createMockService() cloudwatchlogsiface.CloudWatchLogsAPI {
 }
 
 func Test_cloudwatchLogsPlugin_run(t *testing.T) {
+	file, _ := ioutil.TempFile("", "check-cloudwatch-logs-test-run")
+	os.Remove(file.Name())
+	file.Close()
+	defer os.Remove(file.Name())
 	p := &cloudwatchLogsPlugin{
 		Service:      createMockService(),
 		LogGroupName: "test-group",
+		StateFile:    file.Name(),
 	}
 	messages, err := p.run()
 	assert.Equal(t, err, nil, "err should be nil")
 	assert.Equal(t, len(messages), 6)
+	cnt, _ := ioutil.ReadFile(file.Name())
+	var s logState
+	json.NewDecoder(bytes.NewReader(cnt)).Decode(&s)
+	assert.Equal(t, *s.NextToken, "2")
+	assert.Equal(t, *s.StartTime, int64(6))
 }
