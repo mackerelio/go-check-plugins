1d896f27fcdb18a5601db885725d8b3ea2e40c95
tuka <tukaelu@hatena.ne.jp>
2019-06-25 18:03:08 +0900

    [check-procs] If more than one pattern is specified, find processes that meet any of the conditions.

--- check-procs/lib/check_procs.go
+++ check-procs/lib/check_procs.go
@@ -12,29 +12,29 @@ import (
 
 // https://github.com/sensu-plugins/sensu-plugins-process-checks
 var opts struct {
-	WarningOver   *int64  `short:"w" long:"warning-over" value-name:"N" description:"Trigger a warning if over a number"`
-	WarnOver      *int64  `long:"warn-over" value-name:"N" description:"(DEPRECATED) Trigger a warning if over a number"`
-	CritOver      *int64  `short:"c" long:"critical-over" value-name:"N" description:"Trigger a critical if over a number"`
-	WarningUnder  int64   `short:"W" long:"warning-under" value-name:"N" default:"1" description:"Trigger a warning if under a number"`
-	WarnUnder     int64   `long:"warn-under" value-name:"N" default:"1" description:"(DEPRECATED) Trigger a warning if under a number"`
-	CritUnder     int64   `short:"C" long:"critical-under" value-name:"N" default:"1" description:"Trigger a critial if under a number"`
-	MatchSelf     bool    `short:"m" long:"match-self" description:"Match itself"`
-	MatchParent   bool    `short:"M" long:"match-parent" description:"Match parent"`
-	CmdPat        string  `short:"p" long:"pattern" value-name:"PATTERN" description:"Match a command against this pattern"`
-	CmdExcludePat string  `short:"x" long:"exclude-pattern" value-name:"PATTERN" description:"Don't match against a pattern to prevent false positives"`
-	Ppid          string  `long:"ppid" value-name:"PPID" description:"Check against a specific PPID"`
-	FilePid       string  `short:"f" long:"file-pid" value-name:"PID" description:"Check against a specific PID"`
-	Vsz           int64   `short:"z" long:"virtual-memory-size" value-name:"VSZ" description:"Trigger on a Virtual Memory size is bigger than this"`
-	Rss           int64   `short:"r" long:"resident-set-size" value-name:"RSS" description:"Trigger on a Resident Set size is bigger than this"`
-	Pcpu          float64 `short:"P" long:"proportional-set-size" value-name:"PCPU" description:"Trigger on a Proportional Set Size is bigger than this"`
-	Thcount       int64   `short:"T" long:"thread-count" value-name:"THCOUNT" description:"Trigger on a Thread Count is bigger than this"`
-	State         string  `short:"s" long:"state" value-name:"STATE" description:"Trigger on a specific state, example: Z for zombie"`
-	User          string  `short:"u" long:"user" value-name:"USER" description:"Trigger on a specific user"`
-	Usernot       string  `short:"U" long:"user-not" value-name:"USER" description:"Trigger if not owned a specific user"`
-	EsecOver      int64   `short:"e" long:"esec-over" value-name:"SECONDS" description:"Match processes that older that this, in SECONDS"`
-	EsecUnder     int64   `short:"E" long:"esec-under" value-name:"SECONDS" description:"Match process that are younger than this, in SECONDS"`
-	CPUOver       int64   `short:"i" long:"cpu-over" value-name:"SECONDS" description:"Match processes cpu time that is older than this, in SECONDS"`
-	CPUUnder      int64   `short:"I" long:"cpu-under" value-name:"SECONDS" description:"Match processes cpu time that is younger than this, in SECONDS"`
+	WarningOver   *int64   `short:"w" long:"warning-over" value-name:"N" description:"Trigger a warning if over a number"`
+	WarnOver      *int64   `long:"warn-over" value-name:"N" description:"(DEPRECATED) Trigger a warning if over a number"`
+	CritOver      *int64   `short:"c" long:"critical-over" value-name:"N" description:"Trigger a critical if over a number"`
+	WarningUnder  int64    `short:"W" long:"warning-under" value-name:"N" default:"1" description:"Trigger a warning if under a number"`
+	WarnUnder     int64    `long:"warn-under" value-name:"N" default:"1" description:"(DEPRECATED) Trigger a warning if under a number"`
+	CritUnder     int64    `short:"C" long:"critical-under" value-name:"N" default:"1" description:"Trigger a critial if under a number"`
+	MatchSelf     bool     `short:"m" long:"match-self" description:"Match itself"`
+	MatchParent   bool     `short:"M" long:"match-parent" description:"Match parent"`
+	CmdPat        []string `short:"p" long:"pattern" value-name:"PATTERN" description:"Match a command against this pattern"`
+	CmdExcludePat string   `short:"x" long:"exclude-pattern" value-name:"PATTERN" description:"Don't match against a pattern to prevent false positives"`
+	Ppid          string   `long:"ppid" value-name:"PPID" description:"Check against a specific PPID"`
+	FilePid       string   `short:"f" long:"file-pid" value-name:"PID" description:"Check against a specific PID"`
+	Vsz           int64    `short:"z" long:"virtual-memory-size" value-name:"VSZ" description:"Trigger on a Virtual Memory size is bigger than this"`
+	Rss           int64    `short:"r" long:"resident-set-size" value-name:"RSS" description:"Trigger on a Resident Set size is bigger than this"`
+	Pcpu          float64  `short:"P" long:"proportional-set-size" value-name:"PCPU" description:"Trigger on a Proportional Set Size is bigger than this"`
+	Thcount       int64    `short:"T" long:"thread-count" value-name:"THCOUNT" description:"Trigger on a Thread Count is bigger than this"`
+	State         string   `short:"s" long:"state" value-name:"STATE" description:"Trigger on a specific state, example: Z for zombie"`
+	User          string   `short:"u" long:"user" value-name:"USER" description:"Trigger on a specific user"`
+	Usernot       string   `short:"U" long:"user-not" value-name:"USER" description:"Trigger if not owned a specific user"`
+	EsecOver      int64    `short:"e" long:"esec-over" value-name:"SECONDS" description:"Match processes that older that this, in SECONDS"`
+	EsecUnder     int64    `short:"E" long:"esec-under" value-name:"SECONDS" description:"Match process that are younger than this, in SECONDS"`
+	CPUOver       int64    `short:"i" long:"cpu-over" value-name:"SECONDS" description:"Match processes cpu time that is older than this, in SECONDS"`
+	CPUUnder      int64    `short:"I" long:"cpu-under" value-name:"SECONDS" description:"Match processes cpu time that is younger than this, in SECONDS"`
 }
 
 type procState struct {
@@ -76,13 +76,16 @@ func run(args []string) *checkers.Checker {
 	if err != nil {
 		return checkers.NewChecker(checkers.UNKNOWN, err.Error())
 	}
-	cmdPatRegexp := regexp.MustCompile(".*")
-	if opts.CmdPat != "" {
-		r, err := regexp.Compile(opts.CmdPat)
+	var cmdPatRegexp []*regexp.Regexp
+	for _, ptn := range opts.CmdPat {
+		r, err := regexp.Compile(ptn)
 		if err != nil {
 			return checkers.NewChecker(checkers.UNKNOWN, err.Error())
 		}
-		cmdPatRegexp = r
+		cmdPatRegexp = append(cmdPatRegexp, r)
+	}
+	if len(cmdPatRegexp) == 0 {
+		cmdPatRegexp = append(cmdPatRegexp, regexp.MustCompile(".*"))
 	}
 	cmdExcludePatRegexp := regexp.MustCompile(".*")
 	if opts.CmdExcludePat != "" {
@@ -92,27 +95,26 @@ func run(args []string) *checkers.Checker {
 		}
 		cmdExcludePatRegexp = r
 	}
-	var resultrocStates []procState
-	for _, proc := range procs {
-		if matchProc(proc, cmdPatRegexp, cmdExcludePatRegexp) {
-			resultrocStates = append(resultrocStates, proc)
-		}
-	}
-	count := int64(len(resultrocStates))
-	msg := gatherMsg(count)
 	result := checkers.OK
-	if opts.CritUnder != 0 && count < opts.CritUnder ||
-		opts.CritOver != nil && count > *opts.CritOver {
-		result = checkers.CRITICAL
-	} else if opts.WarningUnder != 0 && count < opts.WarningUnder ||
-		opts.WarningOver != nil && count > *opts.WarningOver {
-		result = checkers.WARNING
+	var msg string
+	var resultrocStates []procState
+	for _, reg := range cmdPatRegexp {
+		for _, proc := range procs {
+			if matchProc(proc, reg, cmdExcludePatRegexp) {
+				resultrocStates = append(resultrocStates, proc)
+			}
+		}
+		count := int64(len(resultrocStates))
+		result = optimizeStatus(count, result)
+		msg += fmt.Sprintf("\n%s", gatherMsg(count, reg.String()))
+
+		resultrocStates = []procState{}
 	}
 	return checkers.NewChecker(result, msg)
 }
 
 func matchProc(proc procState, cmdPatRegexp *regexp.Regexp, cmdExcludePatRegexp *regexp.Regexp) bool {
-	return (opts.CmdPat == "" || cmdPatRegexp.MatchString(proc.cmd)) &&
+	return (len(opts.CmdPat) == 0 || cmdPatRegexp.MatchString(proc.cmd)) &&
 		(opts.CmdExcludePat == "" || !cmdExcludePatRegexp.MatchString(proc.cmd)) &&
 		(opts.MatchSelf || proc.pid != strconv.Itoa(os.Getpid())) &&
 		(opts.MatchParent || proc.pid != strconv.Itoa(os.Getppid())) &&
@@ -131,10 +133,10 @@ func matchProc(proc procState, cmdPatRegexp *regexp.Regexp, cmdExcludePatRegexp 
 		(opts.CPUOver == 0 || proc.csec > opts.CPUOver)
 }
 
-func gatherMsg(count int64) string {
+func gatherMsg(count int64, pattern string) string {
 	msg := fmt.Sprintf("Found %d matching processes", count)
-	if opts.CmdPat != "" {
-		msg += fmt.Sprintf("; cmd /%s/", opts.CmdPat)
+	if len(opts.CmdPat) != 0 {
+		msg += fmt.Sprintf("; cmd /%s/", pattern)
 	}
 	if opts.State != "" {
 		msg += fmt.Sprintf("; state /%s/", opts.State)
@@ -176,4 +178,23 @@ func gatherMsg(count int64) string {
 		msg += fmt.Sprintf("; pid %s", opts.FilePid)
 	}
 	return msg
+}
+
+func optimizeStatus(count int64, current checkers.Status) checkers.Status {
+	result := checkers.OK
+	if opts.CritUnder != 0 && count < opts.CritUnder ||
+		opts.CritOver != nil && count > *opts.CritOver {
+		result = checkers.CRITICAL
+	} else if opts.WarningUnder != 0 && count < opts.WarningUnder ||
+		opts.WarningOver != nil && count > *opts.WarningOver {
+		result = checkers.WARNING
+	}
+
+	if result > checkers.WARNING {
+		return checkers.CRITICAL
+	} else if result > checkers.OK {
+		return checkers.WARNING
+	} else {
+		return checkers.OK
+	}
 }
