61ccf60d7746a07f9c81f17c63801407e378a6f8
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-04-23 15:02:27 +0900

    net/url is quoteing the input on Go 1.14

--- check-http/lib/check_http_test.go
+++ check-http/lib/check_http_test.go
@@ -16,13 +16,13 @@ import (
 func TestRun(t *testing.T) {
 	ckr := Run([]string{"-u", "hoge"})
 	assert.Equal(t, ckr.Status, checkers.CRITICAL, "chr.Status should be CRITICAL")
-	assert.Equal(t, ckr.Message, `Get hoge: unsupported protocol scheme ""`, "something went wrong")
+	assert.Equal(t, ckr.Message, `Get "hoge": unsupported protocol scheme ""`, "something went wrong")
 }
 
 func TestNoCheckCertificate(t *testing.T) {
 	ckr := Run([]string{"--no-check-certificate", "-u", "hoge"})
 	assert.Equal(t, ckr.Status, checkers.CRITICAL, "chr.Status should be CRITICAL")
-	assert.Equal(t, ckr.Message, `Get hoge: unsupported protocol scheme ""`, "something went wrong")
+	assert.Equal(t, ckr.Message, `Get "hoge": unsupported protocol scheme ""`, "something went wrong")
 }
 
 func TestStatusRange(t *testing.T) {
