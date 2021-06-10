ea6137eb939cd8de4f6a1d75bbc26a9c40d36461
lufia <lufia@lufia.org>
2021-06-10 18:19:02 +0900

    [ci] run tests on the workflow

---
+++ test.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# prepare tests
+for f in check-*/test.sh
+do
+	dir=$(dirname "$f")
+	name=$(basename "$dir")
+	go build -o "$dir/$name" ./"$dir" || exit
+done
+
+# run tests
+declare -A plugins=()
+declare -a pids=()
+for f in check-*/test.sh
+do
+	./"$f" &
+	pid=$!
+	plugins[$pid]="$f"
+	pids+=("$pid")
+done
+
+# collect the results
+declare -a results=()
+status=0
+for i in "${pids[@]}"
+do
+	if wait "$i"
+	then
+		results+=("OK: ${plugins[$i]}")
+	else
+		results+=("ERR: ${plugins[$i]}")
+		status=1
+	fi
+done
+echo '======' >&2
+for s in "${results[@]}"
+do
+	echo "$s" >&2
+done
+exit $status
