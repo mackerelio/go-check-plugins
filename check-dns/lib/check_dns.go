44ed807c8d9598bb9e6ef3c315ecd0548e8513b9
wafuwafu13 <wafuwafu1313@gmail.com>
2023-02-06 16:56:28 +0900

    to be testable

--- check-dns/lib/check_dns.go
+++ check-dns/lib/check_dns.go
@@ -15,11 +15,18 @@ type dnsOpts struct {
 	Host   string `short:"H" long:"host" required:"true" description:"The name or address you want to query"`
 	Server string `short:"s" long:"server" description:"DNS server you want to use for the lookup"`
 	Port   int    `short:"p" long:"port" default:"53" description:"Port number you want to use"`
+	Norec  bool   `long:"norec" description:"Set not recursive mode"`
 }
 
 // Do the plugin
 func Do() {
-	Run(os.Args[1:])
+	opts, err := parseArgs(os.Args[1:])
+	if err != nil {
+		os.Exit(1)
+	}
+	ckr := opts.run()
+	ckr.Name = "DNS"
+	ckr.Exit()
 }
 
 func parseArgs(args []string) (*dnsOpts, error) {
@@ -28,12 +35,9 @@ func parseArgs(args []string) (*dnsOpts, error) {
 	return opts, err
 }
 
-func Run(args []string) *checkers.Checker {
-	opts, err := parseArgs(args)
-	if err != nil {
-		os.Exit(1)
-	}
+func (opts *dnsOpts) run() *checkers.Checker {
 	var nameserver string
+	var err error
 	if opts.Server != "" {
 		nameserver = opts.Server
 	} else {
@@ -47,10 +51,12 @@ func Run(args []string) *checkers.Checker {
 	c := new(dns.Client)
 	m := &dns.Msg{
 		MsgHdr: dns.MsgHdr{
-			Opcode: dns.OpcodeQuery,
+			RecursionDesired: !opts.Norec,
+			Opcode:           dns.OpcodeQuery,
 		},
 		Question: []dns.Question{{Name: dns.Fqdn(opts.Host), Qtype: dns.TypeA, Qclass: uint16(dns.ClassINET)}},
 	}
+	m.Id = dns.Id()
 
 	r, _, err := c.Exchange(m, nameserver)
 	if err != nil {
--- check-dns/lib/check_dns_test.go
+++ check-dns/lib/check_dns_test.go
@@ -2,6 +2,7 @@ package checkdns
 
 import (
 	"net"
+	"os"
 	"strings"
 	"testing"
 
@@ -21,11 +22,63 @@ func TestNameServer(t *testing.T) {
 	}
 }
 
-func TestDnsStatus(t *testing.T) {
-	ckr := Run([]string{"-H", "example.com", "-s", "8.8.8.8"})
-	assert.Equal(t, checkers.OK, ckr.Status, "should be OK")
-	t.Logf(ckr.Message)
-	if !strings.Contains(ckr.Message, "status: NOERROR") {
-		t.Errorf("status is not NOERROR")
+func TestCheckDns(t *testing.T) {
+	tests := []struct {
+		args        []string
+		want_status checkers.Status
+		want_msg    string
+		local_only  bool
+	}{
+		{
+			[]string{"-H", "example.com"},
+			checkers.OK,
+			"status: NOERROR",
+			true,
+		},
+		{
+			[]string{"-H", "example.com", "--norec"},
+			checkers.OK,
+			"status: NOERROR",
+			true,
+		},
+		{
+			[]string{"-H", "exampleeeee.com"},
+			checkers.CRITICAL,
+			"status: NXDOMAIN",
+			true,
+		},
+		{
+			[]string{"-H", "example.com", "-s", "8.8.8.8"},
+			checkers.OK,
+			"status: NOERROR",
+			false,
+		},
+		{
+			[]string{"-H", "exampleeeee.com", "-s", "8.8.8.8"},
+			checkers.CRITICAL,
+			"status: NXDOMAIN",
+			false,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Logf("=== Start #%d", i)
+		// when runs without setting server in CI, status will be REFUSED
+		if tt.local_only && os.Getenv("RUN_TEST_ON_GITHUB_ACTIONS") == "1" {
+			continue
+		}
+		opts, err := parseArgs(tt.args)
+		if err != nil {
+			t.Fatal(err)
+		}
+		ckr := opts.run()
+
+		assert.Equal(t, tt.want_status, ckr.Status)
+
+		if tt.want_msg != "" {
+			if !strings.Contains(ckr.Message, tt.want_msg) {
+				t.Errorf("%s is not incleded in message", tt.want_msg)
+			}
+		}
 	}
 }
