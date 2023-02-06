116ca0d9bbcd15c36393e105dd3044b736525b1d
wafuwafu13 <wafuwafu1313@gmail.com>
2023-02-06 10:27:35 +0900

    check Rcode

--- check-dns/lib/check_dns.go
+++ check-dns/lib/check_dns.go
@@ -3,14 +3,65 @@ package checkdns
 import (
 	"fmt"
 	"net"
+	"os"
 	"strconv"
+
+	"github.com/jessevdk/go-flags"
+	"github.com/mackerelio/checkers"
+	"github.com/miekg/dns"
 )
 
+type dnsOpts struct {
+	Host   string `short:"H" long:"host" required:"true" description:"The name or address you want to query"`
+	Server string `short:"s" long:"server" description:"DNS server you want to use for the lookup"`
+	Port   int    `short:"p" long:"port" default:"53" description:"Port number you want to use"`
+}
+
+// Do the plugin
 func Do() {
-	nameserver, err := adapterAddress()
+	Run(os.Args[1:])
+}
+
+func parseArgs(args []string) (*dnsOpts, error) {
+	opts := &dnsOpts{}
+	_, err := flags.ParseArgs(opts, args)
+	return opts, err
+}
+
+func Run(args []string) *checkers.Checker {
+	opts, err := parseArgs(args)
 	if err != nil {
-		fmt.Println(err)
+		os.Exit(1)
 	}
-	nameserver = net.JoinHostPort(nameserver, strconv.Itoa(53))
-	fmt.Println(nameserver)
+	var nameserver string
+	if opts.Server != "" {
+		nameserver = opts.Server
+	} else {
+		nameserver, err = adapterAddress()
+		if err != nil {
+			return checkers.Critical(err.Error())
+		}
+	}
+	nameserver = net.JoinHostPort(nameserver, strconv.Itoa(opts.Port))
+
+	c := new(dns.Client)
+	m := &dns.Msg{
+		MsgHdr: dns.MsgHdr{
+			Opcode: dns.OpcodeQuery,
+		},
+		Question: []dns.Question{{Name: dns.Fqdn(opts.Host), Qtype: dns.TypeA, Qclass: uint16(dns.ClassINET)}},
+	}
+
+	r, _, err := c.Exchange(m, nameserver)
+	if err != nil {
+		return checkers.Critical(err.Error())
+	}
+
+	checkSt := checkers.OK
+	if r.MsgHdr.Rcode != dns.RcodeSuccess {
+		checkSt = checkers.CRITICAL
+	}
+	msg := fmt.Sprintf("%s", r)
+
+	return checkers.NewChecker(checkSt, msg)
 }
--- check-dns/lib/check_dns_test.go
+++ check-dns/lib/check_dns_test.go
@@ -2,7 +2,11 @@ package checkdns
 
 import (
 	"net"
+	"strings"
 	"testing"
+
+	"github.com/mackerelio/checkers"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNameServer(t *testing.T) {
@@ -14,5 +18,14 @@ func TestNameServer(t *testing.T) {
 	address := net.ParseIP(nameserver)
 	if address == nil {
 		t.Errorf("nameserver is invalid IP")
+	}
+}
+
+func TestDnsStatus(t *testing.T) {
+	ckr := Run([]string{"-H", "example.com", "-s", "8.8.8.8"})
+	assert.Equal(t, checkers.OK, ckr.Status, "should be OK")
+	t.Logf(ckr.Message)
+	if !strings.Contains(ckr.Message, "status: NOERROR") {
+		t.Errorf("status is not NOERROR")
 	}
 }
